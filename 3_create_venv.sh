3d794b424d002b22650e9997bca7925e611c4f62
Stefan Schmidt <snst@meek.de>
2021-04-05 22:35:05 +0200

    update python packages

--- 3_create_venv.sh
+++ 3_create_venv.sh
@@ -2,3 +2,5 @@ python3 -m venv venv
 source venv/bin/activate
 python3 -m pip install --upgrade pip
 python3 -m pip install -r ./esp-idf/requirements.txt
+pyhton3 -m pip install freetype-py
+pyhton3 -m pip install Image
